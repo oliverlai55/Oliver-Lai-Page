55e44c7d125983e9ef5a24180b4dbbc7c5ffa6e2
Wei lun Lai <oliverlai55@gmail.com>
2015-12-12 17:38:38 -0500

    deleted animated header change

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
 	$('.parallax-window').parallax({imageSrc: 'img/mac.jpg'});
 	console.log("test");
 
+	$(window).scroll(function() {
+		// console.log($(window).scrollTop());
+		console.log("inside window");
+		if ($(document).scrollTop() > 50) {
+			console.log("inside if");
+			$('nav').addClass('shrink');
+		} else {
+			console.log("inside else");
+			$('nav').removeClass('shrink');
+		}
+	});
 
 
 });
