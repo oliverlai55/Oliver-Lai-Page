69d47c064c769465dabe35ef43367cb69a0f83a7
Wei lun Lai <oliverlai55@gmail.com>
2015-12-15 14:11:17 -0500

    adding index2 as an experiment

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,46 @@
 $(document).ready(function(){
-	$('.parallax-window').parallax({imageSrc: 'img/mac.jpg'});
-	console.log("test");
+	// $('.parallax-window').parallax({imageSrc: 'img/mac.jpg'});
+	// console.log("test");
 
-	$(window).scroll(function() {
-		// // console.log($(window).scrollTop());
-		// console.log("inside window");
-		if ($(document).scrollTop() > 50) {
-			$('nav').addClass('shrink');
-		} else {   
-			$('nav').removeClass('shrink');
-		}
+	tiles = $(".profile").fadeTo(0, 0);
+
+	$(window).scroll(function(d,h) {
+		tiles.each(function(i) {
+			a = $(this).offset().top + $(this).height();
+			b = $(window).scrollTop() + $(window).height();
+			if (a < b) $(this).fadeTo(500,1);
+		});
 	});
 
+	// $(window).scroll(function() {
+	// 	// // console.log($(window).scrollTop());
+	// 	// console.log("inside window");
+	// 	if ($(document).scrollTop() > 50) {
+	// 		$('nav').addClass('shrink');
+	// 	} else {   
+	// 		$('nav').removeClass('shrink');
+	// 	}
+	// });
+
 	$('.carousel').carousel({
-		interval: 1000
+		interval: 2000
 	})
 	// console.log("right before wow");
 	// new Wow().init();
 	// console.log('wow initiated');
 
-	var waypoints = $('.about').waypoint({
-		handler: function(){
-		console.log("waypoint alert"); 
-		$('.right-side').attr('right-side wow slideInRight col-sm-4 col-sm-offset-1');
+	// var waypoints = $('.about').waypoint({
+	// 	handler: function(){
+	// 	console.log("waypoint alert"); 
+	// 	$('.right-side').attr('right-side wow slideInRight col-sm-4 col-sm-offset-1');
 
-		$('.left-side').attr('.left-side wow slideInLeft col-sm-6 col-sm-offset-1');
-		// $('.right-side').animate({left: '0px'}, 1000)
-		},
-		offset: '60%'
-	});
-	wow = new WOW();
-	wow.init();
+	// 	$('.left-side').attr('.left-side wow slideInLeft col-sm-6 col-sm-offset-1');
+	// 	// $('.right-side').animate({left: '0px'}, 1000)
+	// 	},
+	// 	offset: '60%'
+	// });
+	// wow = new WOW();
+	// wow.init();
 });
 
 
